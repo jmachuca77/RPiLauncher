4d887b0e78c406809dadae015c8911394da1e50f
jmachuca77 <jaime@element.aero>
2020-05-30 23:55:17 -0600

    add pug

--- app/app.js
+++ app/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'pug');
+
 const server = app.listen(7000, () => {
     console.log(`Express running → PORT ${server.address().port}`);
 });
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.render('index')
 });
